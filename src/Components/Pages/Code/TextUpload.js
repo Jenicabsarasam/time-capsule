4d144c9ae013efa17925fedfe8d07f8343c378c7
Jenica B Sarasam <am.sc.u4cse23323@am.students.amrita.edu>
2024-08-18 14:12:58 +0530

    created text component

--- src/Components/Pages/Code/TextUpload.js
+++ src/Components/Pages/Code/TextUpload.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import '../Styles/Upload.css';
+import { Container,Row,Col,Button} from 'react-bootstrap';
 
 const TextUpload = () => {
+  const validate=()=>{
+    const note=document.getElementById('note').value;
+    if(note.trim()===""){
+      alert('Enter text content!');
+    }
+    else{
+      alert("Text uploaded");
+      clear()
+    }
+  }
+  const clear=()=>{
+    document.getElementById('note').value=""; 
+  }
   return (
-    <div>
-      <h1>Text upload code</h1>
-    </div>
+    <>
+    <Container>
+      <Row>
+        <Col>
+          <input type='text' placeholder='Type something' id='note'/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='outline-success' className='m-3 p-2' onClick={validate}>Upload</Button>
+        </Col>
+        <Col>
+          <Button variant='outline-danger' className='m-3 p-2' onClick={clear}>Cancel</Button>
+        </Col>
+      </Row>
+
+    </Container>
+    </>
   )
 }
 
